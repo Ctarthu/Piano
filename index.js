db6fd4bd04f2e004a55cd88ff11cd7e5fea29700
Saba Abramishvili <grisha.badri@gmail.com>
2021-06-19 20:30:33 +0400

    animation

--- index.js
+++ index.js
@@ -6,7 +6,16 @@
     
     function start(e){
         var audio=document.getElementById(e.key)
+        keyid='#'+e.key+'key'
+        clickAnimation(keyid)
         audio.currentTime=0
         audio.play()
+    }
+    function clickAnimation(keyid){
+        
+        document.querySelector(keyid).setAttribute('style', 'border: 2px solid grey;')
+        setTimeout(function(){document.querySelector(keyid).setAttribute('style', 'border: 1px solid black')}, 1000);
+
+
     }
    
